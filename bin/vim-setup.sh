58028385dcc30404c3d3eb20e1462e1e9b8aa7b9
Suz Hinton <suz.hinton@gmail.com>
2018-09-21 22:38:58 -0700

    pathogen needs to autoload

--- bin/vim-setup.sh
+++ bin/vim-setup.sh
@@ -3,6 +3,7 @@
 BASE_DIR="$HOME/.vim"
 PLUGIN_DIR="$BASE_DIR/bundle"
 COLORS_DIR="$BASE_DIR/colors"
+AUTOLOAD_DIR="$BASE_DIR/autoload"
 PLUGINS=(
   "kien/ctrlp.vim"
   "vim-airline/vim-airline"
@@ -20,6 +21,7 @@ PLUGINS=(
 
 mkdir -p $PLUGIN_DIR
 mkdir -p $COLORS_DIR
+mkdir -p $AUTOLOAD_DIR
 
 wget https://raw.githubusercontent.com/noopkat/dotfiles/master/.vimrc -O $HOME/.vimrc
 wget https://raw.githubusercontent.com/dracula/vim/master/colors/dracula.vim -O $COLORS_DIR/dracula.vim 
@@ -28,5 +30,7 @@ for PLUGIN in ${PLUGINS[@]}; do
    DIRNAME="$(basename $PLUGIN)"
    git clone https://github.com/$PLUGIN.git $PLUGIN_DIR/$DIRNAME
 done
+
+cp $PLUGIN_DIR/vim-pathogen/autoload/pathogen.vim $AUTOLOAD_DIR/pathogen.vim
 
 echo "vim setup done <3"
